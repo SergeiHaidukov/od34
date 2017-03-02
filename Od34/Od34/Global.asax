0cddb51774f009141bb5c99c26e97cfcba404c21
sergey <sergey@USERCOMP>
2017-03-02 13:11:36 +0300

    Main

---
+++ Od34/Od34/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Od34.MvcApplication" Language="C#" %>
---
+++ Od34/Od34/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Od34.Startup))]
+namespace Od34
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
